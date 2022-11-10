8d73e876983dd8cc91eee161dc80db8d1af574e6
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2022-11-11 00:02:25 +0300

    forgot 2 saves

---
+++ week_04/04-javascript-module/03-rps-lesson/activities/02-Stu-RPS/script.js
@@ -0,0 +1,9 @@
+//What do you choose rock, paper or scissors
+//rock beats scissor
+//paper beats rock
+//scissor beat paper
+var r = rock;
+var p = paper;
+var s = scissors;
+var userChoice = prompt('Which do you choose rock, paper or scissors?');
+var computerChoice = Math.random();
