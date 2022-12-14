fc1ee463e2f41cf7b5bb58a9f6ffb9e9fce36202
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2022-12-14 20:51:44 +0000

    basic movie app

--- week_08/08-server-apis-module/movie_app/assets/js/script.js
+++ week_08/08-server-apis-module/movie_app/assets/js/script.js
@@ -1,14 +1,51 @@
 var searchInput = document.querySelector('.search');
 var cardWrapper = document.querySelector('main');
 
+function noMatch(matches) {
+
+}
+function displayMatches (matches) {
+    cardWrapper.innerHTML = '';
+    for (var matchObj of matches) {
+        cardWrapper.insertAdjacentHTML('beforeend', `
+        <div class="movie-card" style='background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${matchObj.movie_image});'>
+            <h3>${matchObj.title}</h3>
+            <p>${matchObj.description} </p>
+            <a href="${matchObj.imdb_link}" target='_blank>'View More info Here</a>
+        </div>
+        `);
+
+    }
+}
+
+
 
 function fetchMovies(event) {
     var keyCode = event.keyCode;
-    var searchText = searchInput.value;
+    var searchText = searchInput.value.toLowerCase().trim();
 
     if (keyCode === 13 && searchText) {
-        console.log('works')
-    }
+        var matches = [];
+
+        for (var movieObj of movieData) {
+
+            if(movieObj.title.toLowerCase().includes(searchText)) {
+                    console.log('match')
+
+            }
+        }
+        
+        searchInput.value = '';
+        displayMatches(matches);
+
+        fetch('https://www.omdbapi.com/?apikey=a085959a&t=jurassic park').then(function (responseObj){
+            var dataPromise = responseObj.json();
+
+            dataPromise.then(function (data) {
+
+            })
+            });
+        }
 }
 
 
@@ -42,8 +79,3 @@ init();
 
 
 
-{/* <div class="movie-card"></div>
-<h3>Movie Title</h3>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
-<a href="#">View More Info Here</a>
-</div> */}
