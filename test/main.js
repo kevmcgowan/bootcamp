70a5fa0b0a9c26f676311f315300ffcb475379cc
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2023-01-17 23:45:31 +0300

    week 11

---
+++ test/main.js
@@ -0,0 +1,19 @@
+// var add = (a, b) => a + b;
+// var sum = add(5, 10);
+
+// var printName = name => console.log(bob);
+
+var data = {
+    name: 'kev',
+    age: 51,
+    hobbies: ['eating', 'drinking'],
+    printName: function() {
+        var capitalize = () => this.name[0].toUpperCase() + this.name.slice(1);
+
+        this.name = capitalize();
+
+        console.log(this.name);
+    }
+};
+data.printName();
+
