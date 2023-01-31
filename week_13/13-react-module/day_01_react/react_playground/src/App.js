78e28ac52f422bcc481f90f2f9bbbb5a65cb135e
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2023-02-01 00:02:38 +0300

    react

---
+++ week_13/13-react-module/day_01_react/react_playground/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div>
+      <header className="My App-Header">
+        <h3>Project</h3>
+        <ul>
+          <li>home</li>
+          <li>dashboard</li>
+        </ul>
+      </header>
+      <main>
+        <h1>this is the start of my REACT journey</h1>
+      </main>
+      <footer>
+        <h2>Where are we going?</h2>
+        <p>&copy; 2023</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
